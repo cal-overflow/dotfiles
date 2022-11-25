52cff483f6fb1146d3f25f03f84106caeb9282b1
Christian Lisle <lisleachristian@gmail.com>
2022-11-25 16:16:33 -0600

    Added undo across neovim sessions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -29,6 +29,9 @@ vim.g.NERDTreeShowHidden=1 -- Show hidden files
 vim.g.NERDTreeHijackNetrw=1
 -- vim.g.NERDTreeShowLineNumbers=0
 
+-- Enable undo across neovim sessions
+vim.opt.undofile=true
+
 -- Require modules
 require('plugins')
 
