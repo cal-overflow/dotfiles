094f9462e1a54573461659ca237da6758cc45b2b
Christian Lisle <42817415+cal-overflow@users.noreply.github.com>
2023-12-10 16:16:45 -0600

    Remove nerdtree & nerdcommenter plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,9 +3,7 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
     -- Utilities
     Plug('jiangmiao/auto-pairs')
-    Plug('preservim/nerdtree')
     Plug('ap/vim-css-color')
-    Plug('preservim/nerdcommenter')
     Plug('godlygeek/tabular')
     Plug('plasticboy/vim-markdown') -- Note tabular MUST be defined before this
     Plug('neoclide/coc.nvim', {branch = 'release'})
@@ -30,17 +28,6 @@ vim.call('plug#begin')
 
 vim.call('plug#end')
 
--- Configure NERDTree - these commands are from the plugin's GitHub repo
-vim.cmd([[
-" Start NERDTree when Vim starts with a directory argument.
-autocmd StdinReadPre * let s:std_in=1
-autocmd VimEnter * if argc() == 1 && isdirectory(argv()[0]) && !exists('s:std_in') |
-    \ execute 'NERDTree' argv()[0] | wincmd p | enew | execute 'cd '.argv()[0] | endif
-]])
-vim.cmd([[
-" Exit Vim if NERDTree is the only window remaining in the only tab.
-autocmd BufEnter * if tabpagenr('$') == 1 && winnr('$') == 1 && exists('b:NERDTree') && b:NERDTree.isTabTree() | quit | endif
-]])
 
 -- Configure vim-markdown plugin
 vim.g.vim_markdown_folding_disabled=1
